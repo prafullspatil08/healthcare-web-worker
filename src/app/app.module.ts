9d8e64325a0290c966d352d352fd76eac4bf4a03
prafullpatil <prafull.patil@mindbowser.com>
2024-07-17 21:05:44 +0530

    Feat: setup application.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgHttpLoaderModule } from 'ng-http-loader';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { PatientsListComponent } from './components/patients-list/patients-list.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientsListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgHttpLoaderModule.forRoot(),
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/patients-list/patients-list.component.ts
@@ -0,0 +1,103 @@
+import { Component, OnInit } from '@angular/core';
+import { PatientDataService } from '../../services/patients-data.service';
+
+@Component({
+  selector: 'app-patients-list',
+  templateUrl: './patients-list.component.html',
+  styleUrls: ['./patients-list.component.scss']
+})
+export class PatientsListComponent implements OnInit {
+
+
+  filteredData: any = [];
+  searchTerm = '';
+  page: number = 1;
+  pageSize: number = 20;
+  originalData: any = [];
+  noRecordsFound: boolean = false;
+  isBusy: boolean = false;
+  webWorkerSeacrh: boolean = false;
+  private worker!: Worker
+
+  constructor(private patientDataService: PatientDataService) {}
+
+  ngOnInit() {
+    this.getClientList();
+  }
+
+  getClientList() {
+    this.patientDataService.getLocalData().subscribe(res => {
+      this.originalData = res;
+      this.filteredData = [...this.originalData]
+    });
+  }
+
+  onSearch() {
+    // if (this.searchTerm) {
+    //   this.filteredData = this.originalData.filter((item: any) => {
+    //     return item.personal_details.first_name.toLowerCase().includes(this.searchTerm.toLowerCase());
+    // });
+    // this.noRecordsFound = this.filteredData.length === 0;
+    // } else {
+    //   this.filteredData = [...this.originalData];
+    //   this.noRecordsFound = false;
+    // }
+    this.isBusy = true;
+    setTimeout(() => {
+      if (this.searchTerm) {
+        const term = this.searchTerm.toLowerCase();
+        const start = performance.now();
+        while (performance.now() - start < 2000) {
+          // Blocking the main thread for 2 seconds
+          this.isBusy = false;
+        }
+        this.filteredData = this.originalData.filter((item: any) => this.containsTerm(item, term));
+        this.noRecordsFound = this.filteredData.length === 0;
+    } else {
+      this.filteredData = [...this.originalData];
+      this.noRecordsFound = false;
+      this.isBusy = false;
+    }
+    }, 1000);
+
+  }
+
+  containsTerm(item: any, term: string): boolean {
+    // If the item is an object, check its values
+    if (item && typeof item === 'object') {
+      return Object.values(item).some(value => this.containsTerm(value, term));
+    }
+    // If the item is a string, check if it includes the search term
+    if (typeof item === 'string') {
+      return item.toLowerCase().includes(term);
+    }
+    // If the item is a number, check if its string representation includes the search term
+    if (typeof item === 'number') {
+      return item.toString().includes(term);
+    }
+    return false;
+  }
+
+
+  filterArrayWithWorker() {
+    this.webWorkerSeacrh = true;
+    // if (typeof Worker !== 'undefined') {
+      this.worker = new Worker(new URL('../../filter.worker.ts', import.meta.url));
+      this.worker.onmessage = ({ data }) => {
+        this.filteredData = data;
+        this.webWorkerSeacrh = false;
+      };
+      const term = this.searchTerm.toLowerCase();
+      this.worker.postMessage({ array: this.originalData, term: term });
+    // } else {
+    //   // Web Workers are not supported in this environment
+    //   this.onSearch();
+    // }
+  }
+
+  alert(message: string) {
+    window.alert(message);
+  }
+
+
+}
